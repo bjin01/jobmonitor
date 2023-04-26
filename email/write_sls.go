db7eecbb85b56987b37f5dc818011234fc5186af
bjin01 <bo.jin@suse.com>
2023-04-26 20:09:52 +0200

    reboot_t7user_datetime

--- email/write_sls.go
+++ email/write_sls.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"text/template"
+	"time"
 
 	"github.com/bjin01/jobmonitor/schedules"
 )
@@ -12,6 +13,8 @@ import (
 func Write_SLS(result *schedules.Jobstatus, templates_dir *Templates_Dir) (string, error) {
 	template_file := fmt.Sprintf("%s/reboot.sls.template", templates_dir.Dir)
 	template, err := template.ParseFiles(template_file)
+	current_time := time.Now()
+	state_file_dir := "/srv/salt/sumapatch"
 	// Capture any error
 	if err != nil {
 		return "", err
@@ -22,21 +25,20 @@ func Write_SLS(result *schedules.Jobstatus, templates_dir *Templates_Dir) (strin
 			return "", err
 		}
 	}
-	if _, err := os.Stat("/srv/salt/sumapatch"); os.IsNotExist(err) {
+	if _, err := os.Stat(state_file_dir); os.IsNotExist(err) {
 		// path/to/whatever does not exist
-		log.Default().Printf("Directory for yaml output file is missing: %s\n", "/srv/salt/sumapatch")
+		log.Default().Printf("Directory for yaml output file is missing: %s\n", state_file_dir)
 		return "", err
 	} else {
-		fileName := fmt.Sprintf("/srv/salt/sumapatch/reboot_%s.sls", result.T7user)
-		f, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+		fileName_path := fmt.Sprintf("%s/reboot_%s_%s.sls", state_file_dir, result.T7user, current_time.Format("20060102150405"))
+		f, err := os.OpenFile(fileName_path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 		if err != nil {
 			return "", err
 		}
 		defer f.Close()
 		template.Execute(f, result)
-		log.Default().Printf("sls file written to %s\n", fileName)
-
+		log.Default().Printf("sls file written to %s\n", fileName_path)
+		fileName := current_time.Format("20060102150405")
 		return fileName, nil
 	}
-
 }
