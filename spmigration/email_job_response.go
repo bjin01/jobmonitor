deebb7ca1a29feb67d983428def9f14da4597481
bjin01 <bo.jin@suse.com>
2023-11-01 19:59:58 +0100

    bugf

--- spmigration/email_job_response.go
+++ spmigration/email_job_response.go
@@ -6,7 +6,7 @@ func (e *Email_job_info) Send_Job_Response_Email(email_job email.Job_Email_Body)
 
 	//here we check if for the same jobid an email has been sent already and avoid sending it again
 	for _, job := range e.Jobinfo {
-		logger.Infof("--- Jobinfo: %+v\n", job)
+		//logger.Infof("--- Jobinfo: %+v\n", job)
 		for jobid, server := range job {
 			/* logger.Infof("--- Jobid: %d Server: %s\n", jobid, server)
 			logger.Info("--- email_job.Job_Response.Job_ID: ", email_job.Job_Response.Job_ID)
@@ -20,7 +20,7 @@ func (e *Email_job_info) Send_Job_Response_Email(email_job email.Job_Email_Body)
 
 	//add the jobid and servername to the jobinfo slice for comparison
 	e.Jobinfo = append(e.Jobinfo, map[int]string{email_job.Job_Response.Job_ID: email_job.Job_Response.Server_name})
-	logger.Infof("Jobinfo: %+v\n", e.Jobinfo)
+	//logger.Infof("Jobinfo: %+v\n", e.Jobinfo)
 
 	//send the email in go routine
 	go email_job.Send_Job_Response()
